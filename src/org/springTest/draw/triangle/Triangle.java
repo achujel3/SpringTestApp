6af95da13c5a33661d549ef0d6afbb01f3dbf224
achujelas123 <achujel3@gmail.com>
2017-04-14 09:49:23 +0300

    testing objects injection

--- src/org/springTest/draw/triangle/Triangle.java
+++ src/org/springTest/draw/triangle/Triangle.java
@@ -4,6 +4,9 @@ public class Triangle {
 
     private String type;
     private int height;
+    private Point pointA;
+    private Point pointB;
+    private Point pointC;
 
     public Triangle(String type, int height) {
         this.type = type;
@@ -34,8 +37,35 @@ public class Triangle {
         this.height = height;
     }
 
+    public Point getPointA() {
+        return pointA;
+    }
+
+    public void setPointA(Point pointA) {
+        this.pointA = pointA;
+    }
+
+    public Point getPointB() {
+        return pointB;
+    }
+
+    public void setPointB(Point pointB) {
+        this.pointB = pointB;
+    }
+
+    public Point getPointC() {
+        return pointC;
+    }
+
+    public void setPointC(Point pointC) {
+        this.pointC = pointC;
+    }
+
     public void draw() {
         System.out.println(this.getHeight() + ", " + this.getType() + ". drawing triangle");
+        System.out.println("PointA: (" + this.getPointA().getX() + ", " + this.getPointA().getY() + ").");
+        System.out.println("PointB: (" + this.getPointB().getX() + ", " + this.getPointB().getY() + ").");
+        System.out.println("PointC: (" + this.getPointC().getX() + ", " + this.getPointC().getY() + ").");
     }
 
 }
