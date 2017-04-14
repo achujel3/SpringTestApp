75a65a558f4a2883409945448d25401364666b71
achujelas123 <achujel3@gmail.com>
2017-04-14 18:05:01 +0300

    testing Collections initializing

--- src/org/springTest/draw/triangle/Triangle.java
+++ src/org/springTest/draw/triangle/Triangle.java
@@ -1,12 +1,20 @@
 package org.springTest.draw.triangle;
 
+import java.util.List;
+
 public class Triangle {
 
     private String type;
     private int height;
-    private Point pointA;
-    private Point pointB;
-    private Point pointC;
+    private List<Point> points;
+
+    public List<Point> getPoints() {
+        return points;
+    }
+
+    public void setPoints(List<Point> points) {
+        this.points = points;
+    }
 
     public Triangle(String type, int height) {
         this.type = type;
@@ -37,35 +45,11 @@ public class Triangle {
         this.height = height;
     }
 
-    public Point getPointA() {
-        return pointA;
-    }
-
-    public void setPointA(Point pointA) {
-        this.pointA = pointA;
-    }
-
-    public Point getPointB() {
-        return pointB;
-    }
-
-    public void setPointB(Point pointB) {
-        this.pointB = pointB;
-    }
-
-    public Point getPointC() {
-        return pointC;
-    }
-
-    public void setPointC(Point pointC) {
-        this.pointC = pointC;
-    }
-
     public void draw() {
         System.out.println(this.getHeight() + ", " + this.getType() + ". drawing triangle");
-        System.out.println("PointA: (" + this.getPointA().getX() + ", " + this.getPointA().getY() + ").");
-        System.out.println("PointB: (" + this.getPointB().getX() + ", " + this.getPointB().getY() + ").");
-        System.out.println("PointC: (" + this.getPointC().getX() + ", " + this.getPointC().getY() + ").");
+        for (Point point : this.getPoints()) {
+            System.out.println("PointA: (" + point.getX() + ", " + point.getY() + ").");
+        }
     }
 
 }
